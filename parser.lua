5d0b7f886487d5d3b30ba0e470f88874589e4928
Raphaël Duhen <raphael.duhen.nexus@gmail.com>
2026-08-07 13:47:15 +0200

    refactor: improve parser code reuse

--- parser.lua
+++ parser.lua
@@ -34,6 +34,41 @@ local function collect_tokens(input)
   end
 end
 
+---@alias ProcessingFunc fun(lexer: Lexer, tok: LexedToken, dst: string, field: string): string?
+---@alias ProcessingTarget {dst: string, field: string}
+
+---@param sequence { tok: Token, process?: [ProcessingTarget, ProcessingFunc] }[]
+---@return string? err
+function Parser:parse_sequence(sequence)
+  for position, step in ipairs(sequence) do
+    local tmp = self.lexer:next()
+
+    -- Error generation
+    if not tmp or tmp.tok ~= step.tok then
+      local err = string.format("Invalid swap syntax. Expected '%s'", lexer_lib.tok_name[step.tok])
+      if not tmp then return err .. "." end
+      if tmp.tok ~= step.tok then
+        err = err .. string.format(" in position %d, got '%s'.", position, self.lexer:from_span(tmp.span))
+      end
+      return err
+    end
+
+    if step.process then
+      local target = step.process[1]
+      local fn = step.process[2]
+      local err = fn(self.lexer, tmp, target[1], target[2])
+      if err then return err end
+    end
+  end
+end
+
+---@type ProcessingFunc
+local function process_number(lexer, tok, dst, field)
+  local str = lexer:from_span(tok.span) --[[@as string]]
+  dst[field] = tonumber(str, 10)
+end
+
+
 ---@return Moveset?, string? error
 function Parser:parse()
   self.lexer:reset()
@@ -57,146 +92,6 @@ function Parser:parse()
 
   local mv_name = string.format("%s - %s", metadata.name, metadata.author)
   return Moveset.new(mv_name, metadata.weapon, swaps or {}, description), nil
-end
-
--- cat ind => cat ind | mod | mod | mod
----@return table<Category, table<Index, [Category, Index, Modifiers]>>? swaps, string? error
-function Parser:parse_swaps()
-  local swaps = {}
-  while self.lexer:peek() do
-    local res, err = self:parse_swap()
-    if not res then return nil, err end
-    if not swaps[res.from[1]] then
-      swaps[res.from[1]] = {}
-    end
-    -- TODO: change swap storage to a simple indexed list
-    -- Runtime cost will be slightly higher but it is what it is.
-    if not swaps[res.from[1]][res.from[2]] then
-      swaps[res.from[1]][res.from[2]] = { res.to[1], res.to[2], res.modifiers }
-    end
-  end
-  return swaps
-end
-
----@return { id: integer, from: [Category, Index], to: [Category, Index], modifiers: Modifiers }?, string? err
-function Parser:parse_swap()
-  local data = {
-    id = 0,
-    from_1 = 0,
-    from_2 = 0,
-    to_1 = 0,
-    to_2 = 0,
-  }
-
-  for _, check in ipairs({
-    { name = "id",     tok = Token.NUMBER },
-    { tok = Token[":"] },
-    { name = "from_1", tok = Token.NUMBER },
-    { name = "from_2", tok = Token.NUMBER },
-    { tok = Token["="] },
-    { tok = Token[">"] },
-    { name = "to_1",   tok = Token.NUMBER },
-    { name = "to_2",   tok = Token.NUMBER },
-  }) do
-    local tmp = self.lexer:next()
-
-    -- Error generation
-    if not tmp or tmp.tok ~= check.tok then
-      local err = string.format("Invalid swap syntax. Expected '%s'", lexer_lib.tok_name[check.tok])
-      if not tmp then return nil, err .. "." end
-      if tmp.tok ~= check.tok then
-        if check.name then
-          err = err .. string.format(" in position '%s'", check.name)
-        end
-        err = err .. string.format(", got '%s'.", self.lexer:from_span(tmp.span))
-      end
-      return nil, err
-    end
-
-    if check.name and data[check.name] then
-      local str = self.lexer:from_span(tmp.span) --[[@as string]]
-      data[check.name] = tonumber(str, 10)
-    end
-  end
-
-  -- Parse modifier
-  ---@type Modifiers
-  local modifiers = {}
-  while true do
-    local peeked = self.lexer:peek()
-    if not peeked or peeked.tok ~= Token["|"] then break end
-    self.lexer:next()
-    local name = self.lexer:next()
-    if not name then
-      return nil, string.format("Unexpected EOF while parsing modifier.", name)
-    end
-    if name.tok ~= Token.IDENTIFIER then
-      return nil, string.format(
-        "Expected IDENTIFIER as modifier name, got '%s' ('%s').",
-        self.lexer:from_span(name.span), lexer_lib.tok_name[name.tok]
-      )
-    end
-    local lower = self.lexer:from_span(name.span):lower()
-    local parse_fn = ({
-      ---@return M_Final
-      final = function() return { enabled = true } end,
-      ---@return M_After?, string? err
-      after = function()
-        local tmp = self.lexer:next()
-        if not tmp or tmp.tok ~= Token["("] then
-          return nil, "Invalid 'After' modifier syntax, expected '('."
-        end
-
-        local id = self.lexer:next()
-        if not id or id.tok ~= Token.NUMBER then
-          return nil, "Invalid 'After' modifier syntax, expected swap ID number."
-        end
-
-        tmp = self.lexer:next()
-        if not tmp or tmp.tok ~= Token[")"] then
-          return nil, "Invalid 'After' modifier syntax, expected ')'."
-        end
-
-        local id_text = self.lexer:from_span(id.span) --[[@as string]]
-        local parsed_id = tonumber(id_text, 10)
-
-        return { enabled = true, prev_id = parsed_id }
-      end
-    })[lower]
-    if not parse_fn then return nil, string.format("Unrecognized modifier '%s'.", self.lexer:from_span(name.span)) end
-    local res, err = parse_fn()
-    if not res then return nil, err end
-    modifiers[lower] = res
-  end
-
-  return { id = data.id, from = { data.from_1, data.from_2 }, to = { data.to_1, data.to_2 }, modifiers = modifiers }
-end
-
----@return string?, string? error
-function Parser:parse_description()
-  ---@type string[]
-  local lines = {}
-  while true do
-    local peeked = self.lexer:peek()
-    if not peeked then return nil, "Unexpected EOF." end
-
-    if peeked.tok == Token["="] then break end
-    if peeked.tok == Token["!"] then self.lexer:skip_line() end
-
-    lines[#lines + 1] = self.lexer:line():trim()
-  end
-  local description = nil
-  for _, line in ipairs(lines) do
-    if #line == 0 then goto continue end
-
-    if description then
-      description = description .. "\n" .. line
-    else
-      description = line
-    end
-    ::continue::
-  end
-  return description
 end
 
 ---@return {name: string, author: string, weapon: Weapon}?, string? error
@@ -270,13 +165,133 @@ function Parser:parse_metadata()
   return metadata
 end
 
--- collect_tokens([[
--- My Moveset - Holo the Drunk
--- Hammer
--- # 2
--- 6 2 37 Final
--- 37 2 6 Final
--- ]])
+---@return string?, string? error
+function Parser:parse_description()
+  ---@type string[]
+  local lines = {}
+  while true do
+    local peeked = self.lexer:peek()
+    if not peeked then return nil, "Unexpected EOF." end
+
+    if peeked.tok == Token["="] then break end
+    if peeked.tok == Token["!"] then self.lexer:skip_line() end
+
+    lines[#lines + 1] = self.lexer:line():trim()
+  end
+  local description = nil
+  for _, line in ipairs(lines) do
+    if #line == 0 then goto continue end
+
+    if description then
+      description = description .. "\n" .. line
+    else
+      description = line
+    end
+    ::continue::
+  end
+  return description
+end
+
+-- cat ind => cat ind | mod | mod | mod
+---@return table<Category, table<Index, [Category, Index, Modifiers]>>? swaps, string? error
+function Parser:parse_swaps()
+  local swaps = {}
+  while self.lexer:peek() do
+    local res, err = self:parse_swap()
+    if not res then return nil, err end
+    if not swaps[res.from[1]] then
+      swaps[res.from[1]] = {}
+    end
+    -- TODO: change swap storage to a simple indexed list
+    -- Runtime cost will be slightly higher but it is what it is.
+    if not swaps[res.from[1]][res.from[2]] then
+      swaps[res.from[1]][res.from[2]] = { res.to[1], res.to[2], res.modifiers }
+    end
+  end
+  return swaps
+end
+
+---@return { id: integer, from: [Category, Index], to: [Category, Index], modifiers: Modifiers }?, string? err
+function Parser:parse_swap()
+  local data = {
+    id = 0,
+    from_1 = 0,
+    from_2 = 0,
+    to_1 = 0,
+    to_2 = 0,
+  }
+
+  local err = self:parse_sequence({
+    { tok = Token.NUMBER, process = { { data, "id" }, process_number } },
+    { tok = Token[":"] },
+    { tok = Token.NUMBER, process = { { data, "from_1" }, process_number } },
+    { tok = Token.NUMBER, process = { { data, "from_2" }, process_number } },
+    { tok = Token["="] },
+    { tok = Token[">"] },
+    { tok = Token.NUMBER, process = { { data, "to_1" }, process_number } },
+    { tok = Token.NUMBER, process = { { data, "to_2" }, process_number } },
+  })
+  if err then return nil, err end
+
+  ---@type Modifiers
+  local modifiers = {}
+  while true do
+    local peeked = self.lexer:peek()
+    if not peeked or peeked.tok ~= Token["|"] then break end
+    self.lexer:next()
+    self:parse_modifier(modifiers)
+  end
+
+  return { id = data.id, from = { data.from_1, data.from_2 }, to = { data.to_1, data.to_2 }, modifiers = modifiers }
+end
+
+---@param modifiers Modifiers
+---@return string? err
+function Parser:parse_modifier(modifiers)
+  local name = self.lexer:next()
+  if not name then
+    return string.format("Unexpected EOF while parsing modifier.", name)
+  end
+  if name.tok ~= Token.IDENTIFIER then
+    return string.format(
+      "Expected IDENTIFIER as modifier name, got '%s' ('%s').",
+      self.lexer:from_span(name.span), lexer_lib.tok_name[name.tok]
+    )
+  end
+  local lower = self.lexer:from_span(name.span):lower()
+  local parse_fn = ({
+    final = self.parse_modifier_final,
+    after = self.parse_modifier_after,
+  })[lower]
+  if not parse_fn then return string.format("Unrecognized modifier '%s'.", self.lexer:from_span(name.span)) end
+
+  local res, error = parse_fn(self)
+  if not res then return error end
+
+  modifiers[lower] = res
+end
+
+---@return M_Final
+function Parser:parse_modifier_final()
+  return { enabled = true }
+end
+
+function Parser:parse_modifier_after()
+  local res = {
+    ---@type integer
+    id = nil
+  }
+
+  local error = self:parse_sequence({
+    { tok = Token["("] },
+    { tok = Token.NUMBER, process = { { res, "id" }, process_number } },
+    { tok = Token[")"] },
+  })
+
+  if error then return nil, error end
+
+  return { enabled = true, prev_id = res.id }
+end
 
 local mv, err = Parser.new([[
 name: My Moveset
